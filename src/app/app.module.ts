518c997c1ad42ee2704d829120027e4682189791
shine <shinesl285@gmail.com>
2020-12-18 11:35:09 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { StudentService } from './student/student.service';
+
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { StudentAddComponent } from './student/student-add.component';
+import { StudentListComponent } from './student/student-list/student-list.component';
+import { StudentViewComponent } from './student/student-view/student-view.component';
+
+const appRoutes: Routes = [
+  { path: 'student-add', component: StudentAddComponent },
+  { path: 'student/:id',      component: StudentViewComponent },
+  {
+    path: 'students',
+    component: StudentListComponent
+  },
+  { path: 'student-add/:id', component: StudentAddComponent },
+  {
+    path: 'students',
+    component: StudentListComponent
+  },
+  { path: '',
+    redirectTo: '/students',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  imports:      [ 
+    BrowserModule, 
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    )
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, StudentAddComponent,StudentViewComponent,StudentListComponent,PageNotFoundComponent ],
+  providers: [StudentService],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
